735d841b5bcf6ffa6380110f8aa8a5bbeb59d20c
John Sullivan <j.sullivan@gmx.com>
2013-04-19 11:25:52 -0700

    Created print_map function.
    * Fixed #1.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+void print_map(char zmap[80][80]) {
+	for (unsigned i = 0; i < 80; ++i) {
+		for (unsigned j = 0; j < 80; ++j) {
+			cout << zmap[i][j];
+		}
+		cout << endl;
+	}
+}
+
 int main()
 {
-	cout << "hello world";
+	cout << "Welcome to glowing-nemesis." << endl;
+
+	char map[80][80];
+	for (unsigned i = 0; i < 80; ++i) {
+		for (unsigned j = 0; j < 80; ++j) {
+			map[i][j] = '.';
+		}
+	}
+
+	print_map(map);
+
 	return 0;
 }
